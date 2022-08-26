616520afea05bfe050380c541e2e6459200d7f33
Muhammet15 <muhammedkayacan3@gmail.com>
2022-08-26 16:06:05 +0300

    migration duzenleme iliski ekleme yapıildi.

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -22,6 +22,7 @@ return new class extends Migration
             $table->timestamp('expires_at')->nullable();
             $table->timestamp('last_used_at')->nullable();
             $table->timestamps();
+            $table->foreign('tokenable_id')->references('user_id')->on('users')->onDelete('cascade');
         });
     }
 
