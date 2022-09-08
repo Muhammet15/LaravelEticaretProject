13eddb802cb8da7c6784192d936942e11f620424
Muhammet15 <muhammedkayacan3@gmail.com>
2022-09-08 13:22:42 +0300

    sadece iyzico kullanımı kaldı geri kalan biti

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -14,17 +14,5 @@ class HomeController extends Controller
         $categories = Category::all()->where("is_active",true);
         return view('frontend.home.index',compact("categories","products"));
     }
-    // public function index($categorySlug = ""){
-    // if(Str::of($categorySlug)->isNotEmpty())
-    // {
-    //     $selectedCategory = Category::all()->where("is_active",true)->where("slug",$categorySlug)->first();
-    //     $products = $selectedCategory->category->where('is_active',true); // modelde ilişki kurarak hallettim.
-    //     //  $products = Product::all()->where("is_active",true)->where("category_id",$selectedCategory->category_id);  
-    // }
-    // else {
-    //  $products = Product::all()->where("is_active",true);
-    // }
-    //     $categories = Category::all()->where("is_active",true);
-    //     return view('frontend.home.index',compact("categories","products"));
-    // }
+  
 }
